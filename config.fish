e055cde29c06785f6677aa36203a84024f70c9f2
Prome-theus <tanishvashisth@gmail.com>
2023-12-20 12:21:13 +0530

    moye moye

---
+++ config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    oh-my-posh init fish | source
+    oh-my-posh init fish --config ~/.config/kitty/tanish.omp.json | source
+end
